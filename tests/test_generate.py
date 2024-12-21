1e4df65d35f06de14e8d23f25ba522226ffad326
Pavlo Penenko <pavlo.penenko@gmail.com>
2024-12-21 15:50:27 -0500

    Integrate `RefDiffer` with the test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import os, sys
 from pathlib import Path
 
 tests_dir_path = Path(__file__).parent
@@ -26,22 +26,35 @@ sys.path += [str(src_dir_path), str(metashade_dir_path)]
 from metashade.util.tests import RefDiffer
 import generate
 
-ref_differ = RefDiffer(
-    ref_dir = repo_root_dir_path / 'tests' / 'ref',
-    out_dir_env_var = 'METASHADE_GLTFSAMPLE_PYTEST_OUT_DIR'
-)
-
 class TestGenerate:
+    @classmethod
+    def setup_class(cls):
+        out_dir = os.getenv('METASHADE_GLTFSAMPLE_PYTEST_OUT_DIR', None)
+        ref_dir = repo_root_dir_path / 'tests' / 'ref' / 'content'
+
+        if out_dir is None:
+            # Don't compare against references explicitly in the script.
+            # Instead, overwrite the references with the generated files.
+            # This is useful for diffing or updating the references manually with
+            # git.
+            cls._out_dir = ref_dir
+            cls._ref_differ = None
+        else:
+            cls._out_dir = Path(out_dir).resolve()
+            cls._ref_differ = RefDiffer(ref_dir)
+
+        os.makedirs(cls._out_dir, exist_ok = True)
+
     def test_generate(self):
         gltf_sample_dir_path = repo_root_dir_path / 'glTFSample'
         gltf_dir_path = gltf_sample_dir_path / 'media' / 'Cauldron-Media'
-        out_dir_path = gltf_sample_dir_path / 'build' / 'DX12' / 'metashade-out'
 
         generate.generate(
             gltf_dir_path = gltf_dir_path,
-            out_dir_path = out_dir_path,
+            out_dir_path = self._out_dir,
             compile = True,
             to_glsl = False,
             skip_codegen = False,
-            serial = False
+            serial = False,
+            ref_differ = self._ref_differ
         )
